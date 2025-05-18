e657737485179d1da858620f3c37d812ef750257
1toe <gnarforall@gmail.com>
2025-05-18 17:50:59 -0400

    refactor: Definir manualmente los tipos en capacitor.config.ts para evitar dependencia de @capacitor/cli

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,5 +1,11 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-   
+// Definir manualmente los tipos para evitar dependencia de @capacitor/cli
+interface CapacitorConfig {
+  appId: string;
+  appName: string;
+  webDir: string;
+  [key: string]: any;
+}
+
 const config: CapacitorConfig = {
   appId: 'com.workprd',
   appName: 'workprd',
